07fd0d346dfb85316c399c99e2b2f31a68282e2e
AbishekBista9 <abhishek.bista@braincast.ai>
2024-01-21 12:48:13 +0545

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
-
+const uri =
+  "mongodb+srv://abishekbista:tEKtoeRxIodBrLn2@cluster0.htfsrbc.mongodb.net/?retryWrites=true&w=majority";
 
 function main() {
-    mongoose.connect(uri).then(() => {
-        console.log("Succesfull")
-    
-    }).catch((err) => {
-        console.log("Error: ", err)
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("Succesfull");
     })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
 }
 
 module.exports = { main };
