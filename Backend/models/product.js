e28fa885dda530c2fb1039a28ca3770c91ccca69
AbishekBista9 <abhishek.bista@braincast.ai>
2024-01-21 17:36:44 +0545

    updates

--- Backend/models/product.js
+++ Backend/models/product.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema(
-  {
-    userID: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'users',
-      required: true,
+// Check if the model already exists
+if (mongoose.models.Product) {
+  module.exports = mongoose.models.Product;
+} else {
+  // Define the model only if it doesn't exist
+  const ProductSchema = new mongoose.Schema(
+    {
+      userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      manufacturer: {
+        type: String,
+        required: true,
+      },
+      stock: {
+        type: Number,
+        required: true,
+      },
+      description: String,
     },
-    name: {
-      type: String,
-      required: true,
-    },
-    manufacturer: {
-      type: String,
-      required: true,
-    },
-    stock: {
-      type: Number,
-      required: true,
-    },
-    description: String,
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+  );
 
-
-const Product = mongoose.model("product", ProductSchema);
-module.exports = Product;
+  const Product = mongoose.model("Product", ProductSchema);
+  module.exports = Product;
+}
